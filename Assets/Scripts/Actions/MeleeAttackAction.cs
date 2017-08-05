f33f3215f5128024a9b54e12d90f07e2fa6024f2
Konstantin Tomashevich <ekit.mail@gmail.com>
2017-08-05 10:20:31 +0300

    Add basic actions data.

---
+++ Assets/Scripts/Actions/MeleeAttackAction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MeleeAttackDirection {
+	UP = 0,
+	DOWN,
+	LEFT,
+	RIGHT
+};
+
+public class MeleeAttackAction : IAction {
+	public IUnit unit;
+	public MeleeAttackDirection direction;
+
+	public MeleeAttackAction (IUnit _unit, MeleeAttackDirection _direction) {
+		unit = _unit;
+		direction = _direction;
+	}
+
+	~MeleeAttackAction () {
+	}
+}
---
+++ Assets/Scripts/Actions/MoveAction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MoveDirection {
+	UP = 0,
+	DOWN,
+	LEFT,
+	RIGHT
+};
+
+public class MoveAction : IAction {
+	public IUnit unit;
+	public MoveDirection direction;
+
+	public MoveAction (IUnit _unit, MoveDirection _direction) {
+		unit = _unit;
+		direction = _direction;
+	}
+
+	~MoveAction () {
+	}
+}
