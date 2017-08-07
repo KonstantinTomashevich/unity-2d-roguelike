6ae43f49f314f142b4c59a4eee23f63714e0e754
Konstantin Tomashevich <ekit.mail@gmail.com>
2017-08-07 19:32:05 +0300

    Map: spawn player on start.

--- Assets/Scripts/Map/Map.cs
+++ Assets/Scripts/Map/Map.cs
@@ -4,14 +4,19 @@ using System.Xml;
 using UnityEngine;
 
 public class Map : MonoBehaviour {
+	public UnitsManager unitsManager;
+	public ItemsManager itemsManager;
+
 	public string mapName;
 	public Color stoneWallColor;
 	public Color stoneFloorColor;
 	public Color woodWallColor;
 	public Color woodFloorColor;
 
-	private Tile[][] tiles;
-	private Texture2D tilesImage;
+	private Tile[][] tiles_;
+	private Texture2D tilesImage_;
+	private XmlDocument mapXml_;
+	private bool isFirstUpdate_;
 
 	public Map () {
 		
@@ -28,16 +33,20 @@ public class Map : MonoBehaviour {
 
 		LoadTiles ();
 		GenerateMesh ();
+		isFirstUpdate_ = true;
 	}
 
 	void Update () {
-		
+		if (isFirstUpdate_) {
+			Init();
+			isFirstUpdate_ = false;
+		}
 	}
 
 	private bool LoadMap () {
-		XmlDocument document = new XmlDocument ();
-		document.Load (Application.dataPath + "/Resources/Maps/" + mapName + "/Map.xml");
-		XmlNode root = document.DocumentElement;
+		mapXml_ = new XmlDocument ();
+		mapXml_.Load (Application.dataPath + "/Resources/Maps/" + mapName + "/Map.xml");
+		XmlNode root = mapXml_.DocumentElement;
 
 		XmlNode tilesInfoNode = root ["tiles"];
 		if (tilesInfoNode.Attributes ["type"].InnerText != "image") {
@@ -46,18 +55,18 @@ public class Map : MonoBehaviour {
 		}
 
 		string imagePath = "Maps/" + mapName + "/" + tilesInfoNode.Attributes ["file"].InnerText;
-		tilesImage = Resources.Load (imagePath) as Texture2D;
-		Debug.Assert (tilesImage != null);
+		tilesImage_ = Resources.Load (imagePath) as Texture2D;
+		Debug.Assert (tilesImage_ != null);
 		return true;
 	}
 
 	private void LoadTiles () {
-		tiles = new Tile[tilesImage.width][];
-		for (int x = 0; x < tilesImage.width; x++) {
-			tiles [x] = new Tile[tilesImage.height];
+		tiles_ = new Tile[tilesImage_.width][];
+		for (int x = 0; x < tiles_.Length; x++) {
+			tiles_ [x] = new Tile[tilesImage_.height];
 
-			for (int y = 0; y < tilesImage.height; y++) {
-				Color color = tilesImage.GetPixel (x, y);
+			for (int y = 0; y < tiles_ [x].Length; y++) {
+				Color color = tilesImage_.GetPixel (x, y);
 				Tile tile = new Tile ();
 
 				if (color == stoneWallColor) {
@@ -83,23 +92,23 @@ public class Map : MonoBehaviour {
 					Debug.LogError ("Unknown tile (" + x + "; " + y + ") with color: " + color.ToString ());
 				}
 					
-				tiles [x] [y] = tile;
+				tiles_ [x] [y] = tile;
 			}
 		}
 	}
 
 	private void GenerateMesh () {
 		Mesh mesh = new Mesh ();
-		Vector2 meshOffset = new Vector2 (-tilesImage.width / 2, -tilesImage.height / 2);
+		Vector2 meshOffset = new Vector2 (-tilesImage_.width / 2, -tilesImage_.height / 2);
 
-		int tilesCount = tilesImage.width * tilesImage.height;
+		int tilesCount = tilesImage_.width * tilesImage_.height;
 		Vector3[] vertices = new Vector3[4 * tilesCount];
 		int[] triangles = new int[6 * tilesCount];
 		Vector2[] uv = new Vector2[4 * tilesCount];
 
-		for (int x = 0; x < tilesImage.width; x++) {
-			for (int y = 0; y < tilesImage.height; y++) {
-				int tileIndex = x * tilesImage.width + y;
+		for (int x = 0; x < tiles_.Length; x++) {
+			for (int y = 0; y < tiles_ [x].Length; y++) {
+				int tileIndex = x * tilesImage_.width + y;
 
 				vertices [tileIndex * 4 + 0] = new Vector3 (-0.5f + x + meshOffset.x, -0.5f + y + meshOffset.y, 0.0f);
 				vertices [tileIndex * 4 + 1] = new Vector3 (0.5f + x + meshOffset.x, -0.5f + y + meshOffset.y, 0.0f);
@@ -113,11 +122,11 @@ public class Map : MonoBehaviour {
 				triangles [tileIndex * 6 + 4] = tileIndex * 4 + 2;
 				triangles [tileIndex * 6 + 5] = tileIndex * 4 + 3;
 
-				Vector2 tileTextureCoord = GetTileTextureCoord (tiles [x] [y].textureIndex);
-				uv [tileIndex * 4 + 0] = new Vector2 (tileTextureCoord.x + 0.01f, tileTextureCoord.y + 0.01f);
-				uv [tileIndex * 4 + 1] = new Vector2 (tileTextureCoord.x + 0.23f, tileTextureCoord.y + 0.01f);
-				uv [tileIndex * 4 + 2] = new Vector2 (tileTextureCoord.x + 0.01f, tileTextureCoord.y + 0.23f);
-				uv [tileIndex * 4 + 3] = new Vector2 (tileTextureCoord.x + 0.23f, tileTextureCoord.y + 0.23f);
+				Vector2 tileTextureCoord = GetTileTextureCoord (tiles_ [x] [y].textureIndex);
+				uv [tileIndex * 4 + 0] = new Vector2 (tileTextureCoord.x + 0.005f, tileTextureCoord.y + 0.005f);
+				uv [tileIndex * 4 + 1] = new Vector2 (tileTextureCoord.x + 0.24f, tileTextureCoord.y + 0.005f);
+				uv [tileIndex * 4 + 2] = new Vector2 (tileTextureCoord.x + 0.005f, tileTextureCoord.y + 0.24f);
+				uv [tileIndex * 4 + 3] = new Vector2 (tileTextureCoord.x + 0.24f, tileTextureCoord.y + 0.24f);
 			}
 		}
 
@@ -134,5 +143,34 @@ public class Map : MonoBehaviour {
 		coord.x = textureX * 0.25f;
 		coord.y = textureY * 0.25f;
 		return coord;
+	}
+
+	private void Init  () {
+		PlacePlayer ();
+	}
+
+	private void PlacePlayer () {
+		XmlNode infoXML = mapXml_.DocumentElement ["player"];
+		Vector2 position = Vector2.zero;
+		position.x = int.Parse (infoXML.Attributes ["positionX"].InnerText);
+		position.y = int.Parse (infoXML.Attributes ["positionY"].InnerText);
+
+		if (bool.Parse (infoXML.Attributes ["invertX"].InnerText)) {
+			position.x = tiles_.Length - position.x;
+		}
+			
+		if (bool.Parse (infoXML.Attributes ["invertY"].InnerText)) {
+			position.y = tiles_ [0].Length - position.y;
+		}
+			
+		if (bool.Parse (infoXML.Attributes ["mapCoords"].InnerText)) {
+			position.x -= tiles_.Length / 2;
+			position.y -= tiles_ [0].Length / 2;
+		}
+
+		Debug.Log (position);
+		PlayerUnit playerUnit = new PlayerUnit ();
+		playerUnit.position = position;
+		unitsManager.AddUnit (playerUnit);
 	}
 }
