addf77f9da68b356fe7ec93dec87d587db089175
Konstantin Tomashevich <ekit.mail@gmail.com>
2017-08-09 21:18:45 +0300

    Add MeleeAttackAnimation and MoveAnimation.

---
+++ Assets/Scripts/Animation/MeleeAttackAnimation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttackAnimation : IAnimation {
+	public const float ANIMATION_TIME = 0.25f;
+
+	private float elapsedTime_;
+	private GameObject sprite_;	
+	private Vector2 speed_;
+
+	public MeleeAttackAnimation (GameObject sprite, Vector2 direction) {
+		elapsedTime_ = 0.0f;
+		sprite_ = sprite;
+		Debug.Assert (direction == Vector2.up || direction == Vector2.down ||
+			direction == Vector2.left || direction == Vector2.right);
+		speed_ = direction / ANIMATION_TIME;
+	}
+
+	~MeleeAttackAnimation () {
+	}
+
+	public void Init (GameObject hostObject) {
+	}
+
+	public void Process () {
+		elapsedTime_ += Time.deltaTime;
+		Vector2 translate = speed_ * Time.deltaTime;
+		if (elapsedTime_ >= ANIMATION_TIME / 2.0f) {
+			translate *= -1;
+		}
+		sprite_.transform.Translate (translate);
+	}
+
+	public bool isFinished { 
+		get {
+			return elapsedTime_ >= ANIMATION_TIME;
+		}
+	}
+}
---
+++ Assets/Scripts/Animation/MoveAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAnimation : IAnimation {
+	public const float ANIMATION_TIME = 0.5f;
+
+	private float elapsedTime_;
+	private GameObject sprite_;	
+	private Vector2 speed_;
+
+	public MoveAnimation (GameObject sprite, Vector2 direction) {
+		elapsedTime_ = 0.0f;
+		sprite_ = sprite;
+		Debug.Assert (direction == Vector2.up || direction == Vector2.down ||
+			direction == Vector2.left || direction == Vector2.right);
+		speed_ = direction / ANIMATION_TIME;
+	}
+
+	~MoveAnimation () {
+	}
+
+	public void Init (GameObject hostObject) {
+	}
+
+	public void Process () {
+		elapsedTime_ += Time.deltaTime;
+		Vector2 translate = speed_ * Time.deltaTime;
+		sprite_.transform.Translate (translate);
+	}
+
+	public bool isFinished { 
+		get {
+			return elapsedTime_ >= ANIMATION_TIME;
+		}
+	}
+}
