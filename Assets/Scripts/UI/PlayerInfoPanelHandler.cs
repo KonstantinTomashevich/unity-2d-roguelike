0aedd7d129a41ae2cc6626cfbf92a31a5be8da7b
Konstantin Tomashevich <ekit.mail@gmail.com>
2017-08-10 21:55:24 +0300

    Add basic player info panel.

---
+++ Assets/Scripts/UI/PlayerInfoPanelHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInfoPanelHandler : MonoBehaviour {
+	public Scrollbar healthBar;
+	public Text healthText;
+	public Text armorText;
+	public Text attackText;
+	public float floorPrecision;
+
+	private IUnit playerUnit_;
+
+	void Start () {
+		playerUnit_ = null;
+	}
+
+	void Update () {
+		if (playerUnit_ != null) {
+			healthBar.size = playerUnit_.health / UnitBase.STANDART_UNIT_MAX_HEALTH;
+			healthText.text = Mathf.Round (playerUnit_.health / floorPrecision) * floorPrecision + " HP";
+			armorText.text = Mathf.Round (playerUnit_.armor / floorPrecision) * floorPrecision + 
+				"    " + Mathf.Floor (1.0f / playerUnit_.moveSpeed) + " moves per turn";
+
+			attackText.text = Mathf.Floor (playerUnit_.attackForce.x / floorPrecision) * floorPrecision + "-" +
+				Mathf.Round (playerUnit_.attackForce.y / floorPrecision) * floorPrecision + " (" +
+				Mathf.Floor (1.0f / playerUnit_.attackSpeed) + " attacks per turn)";
+		}
+	}
+
+	void PlayerUnitCreated (IUnit unit) {
+		playerUnit_ = unit;
+	}
+}
