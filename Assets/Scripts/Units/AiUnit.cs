6878ab0e338f0ab6eaf8d11b00d98c72ebbf822b
Konstantin Tomashevich <ekit.mail@gmail.com>
2017-09-15 20:58:20 +0300

    AiUnit: remove unused variable in NextAction.

--- Assets/Scripts/Units/AiUnit.cs
+++ Assets/Scripts/Units/AiUnit.cs
@@ -11,8 +11,6 @@ public class AiUnit : UnitBase {
 
 	public override IAction NextAction (Map map, UnitsManager unitsManager, ItemsManager itemsManager) {
 		Vector2[] directions = { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
-		Vector2 selectedDirection;
-
 		foreach (Vector2 direction in directions) {
 			IUnit unit = unitsManager.GetUnitOnTile (position + direction);
 			if (unit != null && unit.unitType.Equals ("player")) {
