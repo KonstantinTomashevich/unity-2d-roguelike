ee66ecf50d3e86af44a3d5cbd500e218f8c8f85d
Konstantin Tomashevich <ekit.mail@gmail.com>
2017-10-09 09:27:51 +0300

    Add CargoItem.

---
+++ Assets/Scripts/Items/CargoItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CargoItem : ItemBase {
+	public CargoItem (string itemType) : base (itemType) {
+	}
+
+	~CargoItem () {
+	}
+
+	public override void ProcessTurn (Map map, UnitsManager unitsManager, ItemsManager itemsManager) {
+	}
+}
