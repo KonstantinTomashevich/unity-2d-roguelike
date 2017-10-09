8464f6ac4f7c07b9ce63d5d11eee9e0ec38b0f9b
Konstantin Tomashevich <ekit.mail@gmail.com>
2017-10-09 09:27:39 +0300

    Add CargoItemTypeData.

---
+++ Assets/Scripts/Items/CargoItemTypeData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class CargoItemTypeData : ItemTypeDataBase {
+	public CargoItemTypeData (XmlNode xml, string spritesPathPrefix) : base (xml, spritesPathPrefix) {
+	}
+
+	~CargoItemTypeData () {
+	}
+
+	public override IItem CreateItem (Map map, UnitsManager unitsManager, ItemsManager itemsManager, XmlNode xml) {
+		CargoItem item = new CargoItem (name);
+		InitBasicItemProperties (map, unitsManager, itemsManager, item, xml);
+		return item;
+	}
+}
